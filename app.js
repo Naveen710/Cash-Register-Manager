047d3ab2a1ea797634e43200a01afc7ca8fba9ab
Naveen710 <59551835+Naveen710@users.noreply.github.com>
2022-10-15 17:00:32 +0530

    final commit

--- app.js
+++ app.js
@@ -1,63 +1,80 @@
 const billAmt = document.querySelector("#bill-Amt");
-const billAmtBtn = document.querySelector("#bill-AmtBtn");
+const billAmtBtn = document.querySelector("#billBtn");
+
 const givAmt = document.querySelector("#giv-Amt");
-const givAmtBtn = document.querySelector("#giv-AmtBtn");
+const checkButton = document.querySelector("#giv-AmtBtn");
+
+
+const errorMessage = document.querySelector("#err-message");
+
 const givAmtDisplay = document.querySelector("#giv-amt-display");
-const message = document.querySelector("#err-message");
-const tabValues = document.querySelectorAll(".tab-values");
 const outputTable = document.querySelector("#outputTable");
+
+const tabValues = document.querySelectorAll(".tab-values");
+
+
 const notes = [2000, 500, 100, 20, 10, 5, 1];
 
-function checkBillAmount(){
+function checkBillAmount() {
   hideContent(); // error message needs to removed after user enters correct value
-  const bill = Number(billAmt.value);
-  if (bill > 0) {
-    givAmtDisplay.style.display = "block";
+
+  if (Number(billAmt.value) > 0) {
     billAmtBtn.style.display = "none";
+    givAmtDisplay.style.display = "block";
   } else {
-    showMessage("Please enter a valid bill amount");
+    showMessage("Invalid Bill Amount! Enter a valid Bill Amount");
   }
 };
+
 function checkGivenAmount() {
-  if (
-    !(billAmt.value < 0) &&
-    !(billAmt.value === "") && // checking for both values for the case where user may go back and delete a value
-    !(givAmt.value === "")
-  ) {
-    const bill = Number(billAmt.value);
-    const given = Number(givAmt.value);
-    hideContent(); // error message needs to removed after user enters correct value
-    if (given > bill) {
-      const amount = given - bill;
-      calcDenomination(amount);
-    } else if (given === bill) {
-      showMessage("No need for change!Have a nice day :)");
+  console.log("clicked")
+  console.log("naveen")
+  
+  hideContent();
+  if (Number(billAmt.value) > 0 && Number(givAmt.value) > 0) {
+    if (Number(givAmt.value) >= Number(billAmt.value)) {
+      const amountToBeReturned = Number(givAmt.value) - Number(billAmt.value);
+      if (amountToBeReturned < 1) {
+        console.log(amountToBeReturned)
+        showMessage("No amount should be returned");
+        outputTable.style.display = "none";
+      } else {
+          console.log(amountToBeReturned)
+        calcDenomination(amountToBeReturned);
+      }
     } else {
-      showMessage("The given amount is not sufficient");
+      showMessage("Cash amount should be more than or equal to the bill amount");
     }
   } else {
-    showMessage("Please give proper values for both");
+    showMessage("Invalid input! Enter valid Bill Amount & Cash Given to continue");
+  }
+}
+  function calcDenomination(amount) {
+      outputTable.style.display = "block";
+    for (let i = 0; i < notes.length; i++) {
+      tabValues[i].innerText = Math.floor(amount / notes[i]);
+      amount %= notes[i];
+    }
+    
+  };
+
+  // function clearNoOfNotes() {
+  //   for (let notes of tabValues) {
+  //     notes.innerText = "";
+  //   }
+  // }
+
+  function showMessage(message) {
+    errorMessage.style.display = "block";
+    errorMessage.innerText = message;
+    errorMessage.style.color = "red";
     outputTable.style.display = "none";
-  }
-};
+  };
 
-const showMessage = (msg) => {
-  message.style.display = "block";
-  message.innerText = msg;
-};
+  function hideContent() {
+    errorMessage.style.display = "none";
+    outputTable.style.display = "none";
+  };
 
-const hideContent = () => {
-  message.style.display = "none";
-  outputTable.style.display = "none";
-};
-
-const calcDenomination = (amount) => {
-  for (let i = 0; i < notes.length; i++) {
-    tabValues[i].innerText = Math.floor(amount / notes[i]);
-    amount %= notes[i];
-  }
-  outputTable.style.display = "block";
-};
-
-billAmtBtn.addEventListener("click", checkBillAmount);
-givAmtBtn.addEventListener("click", checkGivenAmount);
+  billAmtBtn.addEventListener("click", checkBillAmount);
+  checkButton.addEventListener("click", checkGivenAmount);
